1d4020575c949deaab3d6334debee4cfe51f5538
marvin.alivio <marvin.alivio@gmail.com>
2025-04-23 00:09:44 +0800

    updated

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -15,7 +15,7 @@ import graphic from '../assets/paint-fill.svg'
 const Page = () => {
     const {data} =  Context();
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 3;
+    const itemsPerPage = 10;
     const navigate = useNavigate()
     const { userLoggedIn } = Context();
     const [isVisible, setIsVisible] = useState(false);
